5b440177abc3ba73fe8fd52bfe70d6f408491a5d
vudung45 <vudung45@yahoo.com>
2019-01-19 19:18:58 +0900

    new config

---
+++ config.py
@@ -0,0 +1,25 @@
+class CONFIG:
+	class SELL_ITEM:
+		username = ""
+		password = ""
+		char_name = "DavidDW"
+		price = 198
+		start_slot = 1
+		num_sell = 10 #unused
+		turns = 12
+		buy_many = False
+		auto_sell = True
+		sell_proxy = True
+		login_proxy = True
+		sleep_time = 1
+		tax = 0.01
+	class VOTE:
+		login_proxy = True
+		sleep_time = 1
+		vote_sleep = 3 #sleep between 2 votes 
+	class GET_BALANCE:
+		login_proxy = True
+		sleep_time = 1
+# usd15
+# 4ae242
+# usd006
